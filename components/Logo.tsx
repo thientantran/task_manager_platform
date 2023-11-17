39aa1f995f88d9ac7559e89d6ed203a4a1112cca
thientantran <thientantm@gmail.com>
2023-11-17 18:41:28 +0700

    ADD FONT for Marketing Page

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,19 @@
+import { cn } from '@/lib/utils';
+import localFont from 'next/font/local';
 import Image from "next/image";
 import Link from "next/link";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2"
+})
+
 
 export default function Logo() {
   return (
     <Link href='/'>
       <div className="md:flex items-center gap-x-2 hidden hover:opacity-75">
         <Image src='/logo.svg' alt="logo" height={30} width={30}/>
-        <p className="text-lg text-neutral-700 pb-1">Taskify</p>
+        <p className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}>Taskify</p>
       </div>
     </Link>
   )
