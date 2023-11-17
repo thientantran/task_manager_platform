186ec4e64a83c6e41db66f61d0b4f82e96c3a140
thientantran <thientantm@gmail.com>
2023-11-17 15:34:00 +0700

    setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-xl text-sky-500">
+      Hello world
+      <Button>Hello</Button>
+    </div>
+  )
+}
