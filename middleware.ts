2c42d2b11642fffc8efb0e80ad8694fa62baea12
thientantran <thientantm@gmail.com>
2023-12-01 19:34:10 +0700

    5. Organization - Logic for Routing when an user sign-in

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,34 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"]
+  publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    // khi vào link, trong 1 số trường hợp thì session đã hết, do đó sẽ được redirect đến trang đăng nhập và do đó, sau khi đăng nhập thành công lại sẽ được redirect vào nơi mà mình mún vào trước đó.
+
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  }
 });
 
 export const config = {
