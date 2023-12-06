32925ab58b0cb7a1536e6a389c26a43b9a8774cc
thientantran <thientantm@gmail.com>
2023-12-06 22:10:05 +0700

    6. Sidebar - Layout

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -3,7 +3,7 @@ import { auth } from "@clerk/nextjs"
 export default function page() {
   const {userId, orgId} = auth()
   return (
-    <div className="pt-16">
+    <div>
       <div>UserId: {userId}</div>
       <div>OrganizationId: {orgId}</div>
     </div>
