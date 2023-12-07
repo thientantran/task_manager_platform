8831a1738ffc1f1b40361ee1127f82371d11ed2c
thientantran <thientantm@gmail.com>
2023-12-07 10:18:21 +0700

    6. SIdebar - Detail Layout
    npx shadcn-ui@latest add accordion
    npx shadcn-ui@latest add separator
    npx shadcn-ui@latest add skeleton
    
    Tạo Layout cho Sidebar và NavItem

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,50 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import NavItem, { Organization } from "./NavItem";
+import { Accordion } from "./ui/accordion";
+import { Skeleton } from "./ui/skeleton";
 
 export default function Sidebar() {
+  const {
+    organization: activeOrganization,
+    isLoaded: isLoadedOrg
+  } = useOrganization();
+  const {
+    userMemberships,
+    isLoaded: isLoadedOrgList
+  } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading){
+    return <>
+      <Skeleton/>
+    </>
+  }
   return (
-    <div>Sidebar</div>
+    <>
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span className="pl-4">
+          Workspaces
+        </span>
+        <Button asChild type="button" size='icon' variant='ghost' className="ml-auto">
+          <Link href='/select-org'>
+            <Plus/>
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+        type="multiple"
+        className="space-y-2"
+      >
+          {userMemberships.data.map(({organization}) => (
+            <NavItem key={organization.id} organization={organization as Organization}/>
+          ))}
+      </Accordion>
+    </>
   )
 }
