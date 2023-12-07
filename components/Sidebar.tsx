930ba041a57de03d29c0371d95e552c2e76f286b
thientantran <thientantm@gmail.com>
2023-12-07 16:54:54 +0700

    7. Skeleton for each section in Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -49,7 +49,17 @@ export default function Sidebar({
 
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading){
     return <>
-      <Skeleton/>
+    {/* skeleton cho header workspace của sidebar */}
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        {/* skeleten cho các navitem */}
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
     </>
   }
   return (
